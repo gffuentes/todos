247d7807e1c8f89a8aa336158f6c3ddcc7b7b323
Gabe Fuentes <gffuentes@gmail.com>
2016-06-01 12:04:30 -0700

    now refactored completions

--- spec/features/user_completes_todo_spec.rb
+++ spec/features/user_completes_todo_spec.rb
@@ -1,18 +1,13 @@
 require 'rails_helper'
 
-#click on link
-#verify 'todo' is completed by referencing completed class in li
-
 feature "User completes todo" do
   scenario "successfully" do
     sign_in
 
-    click_on "Add a new todo"
-    fill_in "Title", with: "Buy milk"
-    click_on "Submit"
+    create_todo "Buy milk"
 
     click_on "Mark Complete"
 
-    expect(page).to have_css '.todos li.completed', text: "Buy milk"
+    expect(page).to display_completed_todo "Buy milk"
   end
 end
